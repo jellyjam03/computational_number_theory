a23126bb63ab2eb4414d41d67348b573c66cd858
Rotariu George <rotariugeorge0@gmail.com>
2025-03-08 11:34:31 +0200

    add fc calculation comparison

--- hw1/main.py
+++ hw1/main.py
@@ -3,6 +3,7 @@ from encode import encode
 from noise import add_noise
 from decode import decode
 from compute_fc import slow_fc, medium_fc, fast_fc
+import time
 
 (a, enc) = encode(29, 11, 1, 1, is_number=True)
 print(a, enc)
@@ -10,8 +11,16 @@ print(a, enc)
 add_noise(enc, 1)
 print(enc)
 
-res = decode(enc, 1, fast_fc, 11)
-print(res)
+fc_methods = {'slow_fc': slow_fc, 'medium_fc': medium_fc, 'fast_fc': fast_fc}
+print('fc calculation -- execution time -- result poly')
+for method in fc_methods:
+    start_time = time.time()
+    res = decode(enc, 1, fc_methods[method], 11)
+    end_time = time.time()
+    print(f'{method} -- {end_time - start_time} -- {res}')
+
+# res = decode(enc, 1, fast_fc, 11)
+# print(res)
 
 # chars = [0b000, 0b001, 0b000, 0b111]
 # (a, enc) = encode(chars, 11, 2, 1, is_number=False)
