983eddcc9c71376fdf624c119b776e3f02fd9e58
Rotariu George <rotariugeorge0@gmail.com>
2025-03-28 11:43:32 +0200

    add addition chain decomposition and fixed window expo

--- rsa/utility.py
+++ rsa/utility.py
@@ -28,16 +28,57 @@ def garner_crt(system):
     return x[-1]
 
 def binary_exp(x, e, m):
+    if x == 1 or x == 0:
+        return x
+
     x %= m
     bits = [int(bit) for bit in bin(e)[2:]]
+    add_chain = []
     res = 1
     for bit in bits:
         res = res * res % m
+        if res > 1:
+            add_chain.append(add_chain[-1] * 2)
         if bit:
-            res = res * x
-    return res
+            res = res * x % m
+            if len(add_chain) != 0:
+                add_chain.append(add_chain[-1] + 1)
+            else:
+                add_chain.append(1)
 
+    return res, add_chain
+
+def to_base_beta(x, beta):
+    beta_digits = []
+    cx = x
+    while cx:
+        beta_digits.append(cx % beta)
+        cx = cx // beta
+    return beta_digits[::-1]
+
+def fixed_window_expo(x, e, beta, m):
+    x %= m
+
+    x_pow = [1]
+    add_chain = []
+    for i in range(1, beta):
+        x_pow.append(x_pow[-1] * x % m)
+        add_chain.append(i)
+
+    beta_digits = to_base_beta(e, beta)
+    res = 1
+    current_pow = 0
+    for digit in beta_digits:
+        res = res ** beta % m
+        current_pow = current_pow * beta
+        if current_pow > add_chain[-1]:
+            add_chain.append(current_pow)
+        res = res * x_pow[digit] % m
+        current_pow = current_pow + digit
+        if current_pow > add_chain[-1]:
+            add_chain.append(current_pow)
+    return res, add_chain
 
 
 if __name__ == '__main__':
-    print(binary_exp(5, 50, 1e40))
+    print(fixed_window_expo(5, 23285, 8, 1e10))
