a3912aaf49d3f374f784c2010e88b5e7ef7a2813
Rotariu George <rotariugeorge0@gmail.com>
2025-03-28 16:17:54 +0200

    refactor and add average runtimes

--- rsa/main.py
+++ rsa/main.py
@@ -4,40 +4,30 @@ import time
 from MultiprimeRSA import *
 from MultipowerRSA import *
 
-p = nextprime(1 << 699)
-# p = nextprime(1 << 20)
-q = nextprime(p)
-r = nextprime(q)
-e = (1 << 16) + 1
-
-
-def multi_prime_demo():
-    rsa = MultiprimeRSA(p, q, r, e)
-    x = rd.randint(1, rsa.n - 1)
-    y = rsa.encrypt(x)
-    print(x, y)
-    start = time.time()
-    fast_res = rsa.fast_decrypt(y)
-    stop = time.time()
-    print(stop - start, fast_res)
-    start = time.time()
-    slow_res = rsa.slow_decrypt(y)
-    stop = time.time()
-    print(stop - start, slow_res)
-
-def multi_power_demo():
-    rsa = MultipowerRSA(p, q, e)
-    x = rd.randint(1, rsa.n - 1)
-    y = rsa.encrypt(x)
-    print(x, y)
-    start = time.time()
-    fast_res = rsa.fast_decrypt(y)
-    print(time.time() - start, fast_res)
-    start = time.time()
-    slow_res = rsa.slow_decrypt(y)
-    print(time.time() - start, slow_res)
+def demo(rsa, samples = 1):
+    fast_res, x = None, None
+    fast_times, slow_times = 0, 0
+    for _ in range(samples):
+        x = rd.randint(1, rsa.n - 1)
+        y = rsa.encrypt(x)
+        start = time.time()
+        fast_res = rsa.fast_decrypt(y)
+        fast_times += time.time() - start
+        start = time.time()
+        slow_res = rsa.slow_decrypt(y)
+        slow_times += time.time() - start
+    print(slow_times / samples, fast_times / samples, slow_times / fast_times)
+    print(f'dec(enc(x) == x ? {fast_res == x}')
 
 if __name__ == '__main__':
-    multi_power_demo()
+    p = nextprime(1 << 699)
+    # p = nextprime(1 << 20)
+    q = nextprime(p)
+    r = nextprime(q)
+    e = (1 << 16) + 1
+
+    multi_prime_rsa = MultiprimeRSA(p, q, r, e)
+    multi_power_rsa = MultipowerRSA(p, q, e)
+    demo(multi_prime_rsa, 30)
     print('-' * 30)
-    multi_prime_demo()
+    demo(multi_power_rsa, 30)
