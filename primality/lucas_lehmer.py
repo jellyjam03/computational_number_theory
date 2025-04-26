60b65c1c5961a4234959392736174f6ec103450b
Rotariu George <rotariugeorge0@gmail.com>
2025-04-26 19:48:04 +0300

    add lucas lehmer test

--- primality/lucas_lehmer.py
+++ primality/lucas_lehmer.py
@@ -8,8 +8,35 @@ def naive_is_prime(n):
         d += 1
     return True
 
+def reduce_mersenne(a, n):
+    m = (1 << n) - 1
+
+    # separate the number into 2 halves
+    a0 = a & m
+    a1 = a >> n
+
+    # reduce the number modulo a
+    a = a0 + a1
+    if a > m:
+        a -= m
+
+    return a if a < m else 0
+
 def lucas_lehmer(n, samples = 10):
-    pass
+    if n == 2:
+        return True
+
+    if not naive_is_prime(n):
+        return False
+
+    u = 4
+    for _ in range(n - 2):
+        u = pow(u, 2) - 2
+        u = reduce_mersenne(u, n)
+
+    return u == 0
 
 if __name__ == '__main__':
-    pass
+    for x in range(60):
+        if lucas_lehmer(x) != naive_is_prime((1 << x) - 1):
+            print(x)
