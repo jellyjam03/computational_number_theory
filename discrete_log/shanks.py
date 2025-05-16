5d615510db77d87b95d5c5741110f932c35ce4d5
Rotariu George <rotariugeorge0@gmail.com>
2025-05-16 20:17:19 +0300

    separate utility functions

--- discrete_log/shanks.py
+++ discrete_log/shanks.py
@@ -1,40 +1,10 @@
 import random as rd
 import math
 from sympy import nextprime, ceiling
+from discrete_log.utility import factorize, get_generator
 
 
 # use Shanks' algorithm to calculate log_alpha(beta) modulo p
-
-def prime_factors(n):
-    res = []
-    d = 2
-    while d * d <= n:
-        if n % d == 0:
-            res.append(d)
-        while n % d == 0:
-            n //= d
-        d += 1
-    if n > 1:
-        res.append(n)
-    return res
-
-
-def get_generator(p):
-    # p prime
-    # returns a generator element of the group Zp
-    phi = p - 1
-    fact = prime_factors(phi)
-    for g in range(2, p):
-        # is g a generator of Zp?
-        # phi = p1^e1 * p2^e2 * ... * pk^ek
-        # g is a generator if g^(phi(p) / pi) != 1, i = 1, ..k
-
-        for factor in fact:
-            if pow(g, phi // factor, p) == 1:
-                break
-        else:
-            return g
-    return None
 
 def binary_search(v, x):
     lo, hi = -1, len(v)
@@ -47,8 +17,6 @@ def binary_search(v, x):
     if hi == len(v):
         hi -= 1
     return v[hi]
-
-
 
 def shanks(alpha, beta, p):
     # p, a prime
@@ -72,7 +40,8 @@ def shanks(alpha, beta, p):
 
 if __name__ == '__main__':
     p = nextprime((1 << 32))
-    alpha = get_generator(p)
+    fact, _ = factorize(p - 1)
+    alpha = get_generator(p, fact)
     beta = rd.randint(1, p)
 
     epsilon = shanks(alpha, beta, p)
---
+++ discrete_log/utility.py
@@ -0,0 +1,51 @@
+from sympy import isprime, randprime
+
+def factorize(n):
+    fact = []
+    pows = []
+    d = 2
+    while d * d <= n:
+        if n % d == 0:
+            fact.append(d)
+        pows.append(0)
+        while n % d == 0:
+            pows[-1] += 1
+            n //= d
+        d += 1
+    if n > 1:
+        fact.append(n)
+        pows.append(1)
+    return fact, pows
+
+def get_generator(p, fact):
+    # p prime
+    # returns a generator element of the group Zp
+    phi = p - 1
+    for g in range(2, p):
+        # is g a generator of Zp?
+        # phi = p1^e1 * p2^e2 * ... * pk^ek
+        # g is a generator if g^(phi(p) / pi) != 1, i = 1, ..k
+
+        for factor in fact:
+            if pow(g, phi // factor, p) == 1:
+                break
+        else:
+            return g
+    return None
+
+def generate_smooth_prime(bit_length=1024, max_factor_bits=64):
+    # Try different sets of small primes until we find a good one
+    while True:
+        factors = []
+        current_product = 2  # start with 2 to ensure even p - 1
+        while current_product.bit_length() < bit_length - 20:
+            q = randprime(3, 2 ** max_factor_bits)
+            if q not in factors:  # avoid duplicates
+                factors.append(q)
+                current_product *= q
+
+        # Try small odd multipliers k to get a candidate p
+        for k in range(1, 10000, 2):
+            p_candidate = current_product * k + 1
+            if p_candidate.bit_length() >= bit_length and isprime(p_candidate):
+                return p_candidate, sorted(factors + [k]), [1 for _ in range(len(factors))]
