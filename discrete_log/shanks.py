e1f559cbd793147216cb596ddf57e5fa447c76c9
Rotariu George <rotariugeorge0@gmail.com>
2025-05-16 11:28:12 +0300

    add shanks algorithm

--- discrete_log/shanks.py
+++ discrete_log/shanks.py
@@ -1,5 +1,8 @@
 import random as rd
-from sympy import nextprime
+import math
+from sympy import nextprime, ceiling
+
+
 # use Shanks' algorithm to calculate log_alpha(beta) modulo p
 
 def prime_factors(n):
@@ -31,22 +34,46 @@ def get_generator(p):
                 break
         else:
             return g
-
     return None
+
+def binary_search(v, x):
+    lo, hi = -1, len(v)
+    while hi - lo > 1:
+        mid = (lo + hi) // 2
+        if v[mid][1] >= x:
+            hi = mid
+        else:
+            lo = mid
+    if hi == len(v):
+        hi -= 1
+    return v[hi]
+
+
 
 def shanks(alpha, beta, p):
     # p, a prime
     # alpha, a primitive root of Zp
     # beta, a member of Zp
+    m = int(ceiling(math.sqrt(p - 1)))
+    inv = pow(alpha, -m, p)
 
-    return None
+    # baby steps
+    L = [(j, pow(alpha, j, p)) for j in range(0, m)]
+    L = sorted(L, key = lambda x: x[1])
+
+    # giant steps
+    for i in range(0, m):
+        x = (beta * pow(inv, i, p)) % p
+        # search for x in pr2(L)
+        res = binary_search(L, x)
+        if res[1] == x:
+            # found x in L
+            return i * m + res[0]
 
 if __name__ == '__main__':
-    # p = nextprime((1 << 32))
-    # alpha = get_generator(p)
-    # beta = rd.randint(1, p)
-    #
-    # epsilon = shanks(alpha, beta, p)
-    # print(epsilon, pow(alpha, epsilon, p))
-    print(get_generator(17))
-    pass
+    p = nextprime((1 << 32))
+    alpha = get_generator(p)
+    beta = rd.randint(1, p)
+
+    epsilon = shanks(alpha, beta, p)
+    print(epsilon, beta, pow(alpha, epsilon, p))
